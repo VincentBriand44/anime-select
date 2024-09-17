73c424e9aadcd90c257abba555677c8470b741f3
VincentBriand44 <vincent-briand_student2022@wilder.school>
2024-09-17 19:01:02 +0200

    :recycle: refactoring layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,7 @@ import '../app.pcss';
 
 <div class="bg-primary h-screen w-screen p-8 flex relative">
   <Sidebar />
-  <main class="max-w-screen-xl xl:max-w-screen-lg w-full mx-auto">  
+  <main class="bg-secondary flex justify-between flex-col rounded-3xl max-w-screen-xl xl:max-w-screen-lg w-full mx-auto h-full p-4 shadow-2xl text-secondary">  
     <slot />
   </main>
 </div>
