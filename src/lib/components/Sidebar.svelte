b4cf872d3a8c789c427165e5474e80113bcf2cec
VincentBriand44 <vincent-briand_student2022@wilder.school>
2024-09-17 17:54:00 +0200

    :dizzy: sidebar animation

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-import { Bars3, Icon } from 'svelte-hero-icons';
+import { Bars3, Icon, XMark } from 'svelte-hero-icons';
 
 interface Nav {
   name: string;
@@ -17,21 +17,22 @@ const nav: Nav[] = [
   },
 ];
 
-let open = true;
+let open = false;
 </script>
 
 <div class="absolute left-8 top-8 h-10 w-10 flex justify-end items-end xl:bg-transparent bg-secondary rounded-br-full z-10" class:bg-secondary={!open}>
   <button
-    class="absolute -left-4 -top-4 rounded-full bg-primary saturate-150 h-10 w-10 shadow-xl hover:shadow-sm p-2"
+    class="absolute -left-4 -top-4 rounded-full bg-primary saturate-150 h-10 w-10 shadow-xl hover:shadow-md p-2"
     on:click={()=>{open = !open}}
   >
-    <Icon src="{Bars3}" class="text-primary" />
+    <Icon src="{open ? XMark : Bars3}" class="text-primary" />
   </button>
 </div>
 
 {#if open}
-  <nav class="fixed w-64 h-screen pt-20 p-6 xl:relative left-0 top-0 bg-primary self-start">
-    <ul>
+  <nav class="fixed w-64 h-screen xl:relative left-0 top-0 self-start overflow-hidden p-4">
+    <div class="bg-primary h-10 w-10 rounded-full"></div>
+    <ul class="fixed p-6 pt-16 top-0 left-0 w-64 h-screen opacity-0 xl:opacity-100">
       {#each nav as { name, href }}
       <li class="text-primary font-bold"><a {href}>{name}</a></li>
       {/each}
@@ -39,4 +40,33 @@ let open = true;
   </nav>
 {/if}
 
-<!-- Créer une animation d'ouverture avec un cercle (visible en version fixed) -->
+<style lang="postcss">
+  @media screen and (max-width: 1280px) {
+    nav > div {
+      animation: scale 1s;
+    }
+    nav > ul {
+      animation: display 1s forwards;
+    }
+  }
+
+ @keyframes scale {
+  2% { 
+    @apply scale-[5]
+  }
+  100% { 
+    @apply scale-[50]
+  }
+ }
+ @keyframes display {
+  25% {
+    @apply opacity-0
+  }
+  75% {
+    @apply bg-transparent shadow-none
+  }
+  100% {
+    @apply opacity-100 bg-primary shadow-2xl
+  }
+ }
+</style>
