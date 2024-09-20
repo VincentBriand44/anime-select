527f2e8721a30433c2c72ad5e83a7092aff637f4
VincentBriand44 <vincent-briand_student2022@wilder.school>
2024-09-20 19:34:47 +0200

    :sparkles: implement load spinner

--- src/routes/preview/+page.svelte
+++ src/routes/preview/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 import Preview from '$lib/components/Preview.svelte';
 import Jikan from 'jikan4.js';
+import { Shadow } from 'svelte-loading-spinners';
 
 const client = new Jikan.Client();
 
@@ -8,9 +9,12 @@ let error: string | null = null;
 </script>
 
 {#await client.seasons.getNow()}
-  <p class="m-auto">Chargement des données...</p>
+  <div class="flex gap-4 m-auto items-center">
+    <Shadow size="1.3" color="#181D31" unit="rem" duration="1.5s" />
+    <p class="text-secondary">Chargement des données...</p>
+  </div>
 {:then value}
-  <Preview season={setTimeout(()=>{}/*value.sort((a, b) => b.popularity - a.popularity)*/, 60 * 1000)} />
+  <Preview season={value.sort((a, b) => b.popularity - a.popularity)} />
 {:catch error}
   <p class="m-auto">Un problème est survenu lors de la récupération des données</p>
 {/await}
