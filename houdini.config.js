a91500096c1232427b32cc15d6cab3525b56ba0b
VincentBriand44 <vincent-briand_student2022@wilder.school>
2024-09-21 17:25:58 +0200

    :adhesive_bandage: ignore type in client.ts

--- houdini.config.js
+++ houdini.config.js
@@ -2,12 +2,12 @@
 
 /** @type {import('houdini').ConfigFile} */
 const config = {
-    "watchSchema": {
-        "url": "https://api.anemy.fr/v3/"
-    },
-    "plugins": {
-        "houdini-svelte": {}
-    }
-}
+  watchSchema: {
+    url: 'https://api.anemy.fr/v3/',
+  },
+  plugins: {
+    'houdini-svelte': {},
+  },
+};
 
-export default config
+export default config;
--- src/client.ts
+++ src/client.ts
@@ -1,15 +1,6 @@
+// @ts-ignore
 import { HoudiniClient } from '$houdini';
 
 export default new HoudiniClient({
-    url: 'https://api.anemy.fr/v3/'
-
-    // uncomment this to configure the network call (for things like authentication)
-    // for more information, please visit here: https://www.houdinigraphql.com/guides/authentication
-    // fetchParams({ session }) {
-    //     return {
-    //         headers: {
-    //             Authentication: `Bearer ${session.token}`,
-    //         }
-    //     }
-    // }
-})
+  url: 'https://api.anemy.fr/v3/',
+});
